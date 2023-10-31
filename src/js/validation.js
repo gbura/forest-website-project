bcf836e8aa6839f145467e2901ac6f15cbe81a3c
gbura <rage6999@wp.pl>
2023-10-31 19:25:41 +0100

    added popup and fixed validation

--- src/js/validation.js
+++ src/js/validation.js
@@ -6,22 +6,32 @@ const msgBox = document.querySelector('.msg')
 const sendBtn = document.querySelector('.btn-send')
 const clearBtn = document.querySelector('.btn-clear')
 
+const popup = document.querySelector('.popup')
+
 // email validation
 const checkEmail = () => {
 	const re =
 		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/
 
-	if (re.test(inputMail.value) && (inputMail.value != '' || inputName.value != '' || msgBox.value != '')) {
+	if (re.test(inputMail.value)) {
 		clearError()
+		showPopup()
+		clearInputs()
 	} else {
 		showError()
 	}
 }
 
+const clearInputs = () => {
+	inputName.value = ''
+	inputMail.value = ''
+	msgBox.value = ''
+}
+
 // showing error if data is wrong
 const showError = () => {
 	error.classList.add('active')
-	error.innerHTML = 'Podałeś błędne dane! Spróbuj ponownie!'
+	error.innerHTML = 'Podałeś/aś błędne dane! Spróbuj ponownie!'
 }
 
 // clearing errors if data is fine
@@ -32,11 +42,27 @@ const clearError = () => {
 
 // function that clears every field in form
 const clearForm = () => {
-	inputName.value = ''
-	inputMail.value = ''
-	msgBox.value = ''
+	clearInputs()
 	clearError()
 }
 
+const showPopup = () => {
+	if (inputMail.value != '' || inputName.value != '' || msgBox.value != '') {
+		popup.classList.add('success')
+		popup.textContent = 'Wysłano formularz'
+		setTimeout(() => {
+			popup.classList.remove('success')
+		}, 2000)
+	}
+}
+
 sendBtn.addEventListener('click', checkEmail)
-clearBtn.addEventListener('click', clearForm)
+
+clearBtn.addEventListener('click', () => {
+	clearForm()
+	popup.classList.add('clear')
+	popup.textContent = 'Wyczyszczono formularz'
+	setTimeout(() => {
+		popup.classList.remove('clear')
+	}, 2000)
+})
